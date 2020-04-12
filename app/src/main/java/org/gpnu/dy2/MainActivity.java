dc6c7588f7e1c3d9557243c8c33a92cc9c167c85
Purcell Huang <purcellhuang@163.com>
2020-04-12 20:14:37 +0800

    修复了一些BUG

--- app/src/main/java/org/gpnu/dy2/MainActivity.java
+++ app/src/main/java/org/gpnu/dy2/MainActivity.java
@@ -1,9 +1,5 @@
 package org.gpnu.dy2;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
@@ -14,12 +10,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.amap.api.maps.AMap;
 import com.amap.api.maps.CameraUpdateFactory;
 import com.amap.api.maps.MapView;
 import com.amap.api.maps.model.BitmapDescriptorFactory;
-import com.amap.api.maps.model.Circle;
-import com.amap.api.maps.model.CircleOptions;
 import com.amap.api.maps.model.LatLng;
 import com.amap.api.maps.model.LatLngBounds;
 import com.amap.api.maps.model.MarkerOptions;
@@ -34,6 +32,9 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, AMap.OnMapClickListener {
+    private static final String TAG = "MainActivity";
+
+    private boolean flag = false;    //标记当前是否在设置航线
     private boolean isFlying = false;
 
     private Button bt_fly;
@@ -54,7 +55,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private final int second = 10;
 
     private List<LatLng> points = new ArrayList<>();
-    private static final String TAG = "MainActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,7 +80,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mMapView.onCreate(savedInstanceState);
 
         aMap = mMapView.getMap();
-        aMap.setOnMapClickListener(this);
+
+        bt_fly.setEnabled(false);
+        bt_fly.setTextColor(getResources().getColor(R.color.buttonInactive));
+        bt_reset_plane.setEnabled(false);
+        bt_reset_plane.setTextColor(getResources().getColor(R.color.buttonInactive));
 
     }
 
@@ -109,47 +114,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
-    private void flying() {
-        if (planeMarker!=null){
-            planeMarker.destroy();
-        }
-        LatLngBounds bounds = new LatLngBounds(points.get(0), points.get(points.size() - 2));
-        aMap.animateCamera(CameraUpdateFactory.newLatLngBounds(bounds, 50));
+    private void startFly() {
 
-        setPlanePlace();
-
-        // 设置滑动的总时间
+        // 设置飞行的总时间
         planeMarker.setTotalDuration(second);
-        // 开始滑动
+        // 开始飞行
         planeMarker.startSmoothMove();
 
+        //定时 每0.3秒查看当前经纬度及高度
         myTask = new MyTask();
-
-        //定时查看当前经纬度及高度
         timer.schedule(myTask, 0, 300);
-
-        planeMarker.setMoveListener(new SmoothMoveMarker.MoveListener(){
-            @Override
-            public void move(double v) {
-//                Log.i("distance",planeMarker.getPosition().toString());
-            }
-        });
 
     }
 
     /**
-     * 飞机起点
+     * 初始化起飞起点
      */
-    private void setPlanePlace() {
+    private void initPlane() {
         planeMarker = new SmoothMoveMarker(aMap);
-        // 设置滑动的图标
         planeMarker.setDescriptor(BitmapDescriptorFactory.fromResource(R.drawable.plane));
         LatLng drivePoint = points.get(0);
         Pair<Integer, LatLng> pair = SpatialRelationUtil.calShortestDistancePoint(points, drivePoint);
         points.set(pair.first, drivePoint);
         List<LatLng> subList = points.subList(pair.first, points.size());
-
-        // 设置滑动的轨迹左边点
         planeMarker.setPoints(subList);
     }
 
@@ -157,59 +144,80 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.bt_fly://开始飞行事件
-                aMap.setOnMapClickListener(null);
+                if (points.isEmpty()) {
+                    break;
+                }
 
-                showPolyline();
+                isFlying = true;
 
                 bt_fly.setEnabled(false);
                 bt_fly.setTextColor(getResources().getColor(R.color.buttonInactive));
-
+                bt_reset_plane.setEnabled(true);
+                bt_reset_plane.setTextColor(getResources().getColor(R.color.buttonActive));
                 bt_reset_route.setEnabled(false);
                 bt_reset_route.setTextColor(getResources().getColor(R.color.buttonInactive));
-                flying();
+                startFly();
 
-                isFlying = true;
                 break;
 
             case R.id.bt_reset_plane://重置飞机事件
-                myTask.cancel();
-                isFlying = false;
+                if (isFlying) {
+                    myTask.cancel();
 
-                //停止飞行
-//                planeMarker.setVisible(false);
-                planeMarker.destroy();
+                    //重置飞机
+                    planeMarker.destroy();
+                    initPlane();
 
-                setPlanePlace();    //回到起点
+                    tv_info.setText(String.format("经度：%.4f,纬度：%.4f,高度：0米", points.get(0).longitude, points.get(0).latitude));
 
-                //飞机停止 重置航线可用 、飞机可以起飞
-                bt_reset_route.setEnabled(true);
-                bt_reset_route.setTextColor(getResources().getColor(R.color.buttonActive));
-                bt_fly.setEnabled(true);
-                bt_fly.setTextColor(getResources().getColor(R.color.buttonActive));
-
+                    //飞机停止 重置航线可用 、飞机可以起飞
+                    bt_reset_route.setEnabled(true);
+                    bt_reset_route.setTextColor(getResources().getColor(R.color.buttonActive));
+                    bt_fly.setEnabled(true);
+                    bt_fly.setTextColor(getResources().getColor(R.color.buttonActive));
+                }
                 break;
             case R.id.bt_reset_route://重置航线事件
-                //清除地图上的Marker和Line
-                points.clear();
-                aMap.clear();
 
-                points.clear();
-                isFlying = false;
-                aMap.setOnMapClickListener(this);
+                if (flag) {  //表示确认航线
+                    bt_reset_route.setText("重置航线");
+
+                    aMap.setOnMapClickListener(null);
+
+                    initPlane();
+                    showPolyline();
+
+                    bt_fly.setEnabled(true);
+                    bt_fly.setTextColor(getResources().getColor(R.color.buttonActive));
+                    flag = false;
+                } else {
+                    //清除地图上的Marker和Line
+                    points.clear();
+                    aMap.clear();
+
+                    bt_reset_route.setText("确认");
+
+                    bt_fly.setEnabled(false);
+                    bt_fly.setTextColor(getResources().getColor(R.color.buttonInactive));
+                    bt_reset_plane.setEnabled(false);
+                    bt_reset_plane.setTextColor(getResources().getColor(R.color.buttonInactive));
+
+                    aMap.setOnMapClickListener(this);
+                    flag = true;
+                }
+
                 break;
         }
     }
 
     /**
      * 标记点击的位置
+     *
      * @param latLng
      */
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     @Override
     public void onMapClick(LatLng latLng) {
-
-        Log.i("distance",latLng.toString());
-
         MarkerOptions markerOptions = new MarkerOptions();
         markerOptions.position(latLng);
         TextView textView = new TextView(this);
@@ -218,15 +226,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         textView.setText(String.valueOf(points.size()));
         markerOptions.icon(BitmapDescriptorFactory.fromView(textView));
         markerOptions.title(String.valueOf(points.size()));
-        markerOptions.anchor(0.5f, 0.5f);
-        points.add(latLng);
-
+        markerOptions.anchor(0.5f, 0.5f);//icon居中
         aMap.addMarker(markerOptions);
 
-        if (points.size() > 0) {
-            bt_fly.setEnabled(true);
-            bt_fly.setTextColor(getResources().getColor(R.color.buttonActive));
-        }
+        points.add(latLng);
+
     }
 
     private Polyline showPolyline() {
@@ -234,15 +238,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 addAll(points).width(6).color(Color.argb(255, 1, 1, 1)));
     }
 
-    class MyTask extends TimerTask{
-        int mill = second*1000;
+    class MyTask extends TimerTask {
+        int mill = second * 1000;
+
         @Override
         public void run() {
             LatLng latLng = planeMarker.getPosition();
-            Log.i("distance",planeMarker.getPosition().toString());
-            tv_info.setText(String.format("经度：%.4f,纬度：%.4f,高度：0", latLng.longitude,latLng.latitude));
+            Log.i("distance", planeMarker.getPosition().toString());
+            tv_info.setText(String.format("经度：%.4f,纬度：%.4f,高度：%.2f米", latLng.longitude, latLng.latitude, Math.random() * 1000));
             mill = mill - 300;
-            if (mill < 0){  //飞行结束
+            if (mill < 0) {  //飞行结束
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
